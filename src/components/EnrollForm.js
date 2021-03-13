0ece5a6bc23351f2f17fac107c7e241f525fbf2f
erkovacs <kovaroberik@gmail.com>
2021-03-13 23:30:21 +0200

    managed to move the functionaliy to context

--- src/components/EnrollForm.js
+++ src/components/EnrollForm.js
@@ -1,5 +1,6 @@
 import { Card, Form, Button } from 'react-bootstrap'
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
+import { Web3Context } from './Web3Context';
 import './Form.css';
 
 const GENDERS = [
@@ -8,86 +9,81 @@ const GENDERS = [
   'other'
 ];
 
-class EnrollForm extends Component {
+const EnrollForm = props => {
 
-  constructor (props) {
-    super(props);
-    this.state = {
-      account: props.data.account
-    };
-  }
+  const { data } = useContext(Web3Context);
+  const [state, setState] = useState({
+    account: data.account
+  });
 
-  handleChange (e) {
-    this.setState({ 
+  const handleChange = e => {
+    setState({ 
       [e.target.id]: e.target.value 
     });
   }
 
-  onSubmit (e) {
+  const onSubmit = e => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
+    console.log(state);
   }
-
-  render() {
-    return (
-      <div>
-        <br></br>
-        <Card>
-          <Card.Body>
-            <Card.Title>Patient Enrollment for Study participation</Card.Title>
-            <Form>
-              <br></br>
-              <Form.Group controlId="walletAccount">
-                <Form.Label>Patient Wallet address: </Form.Label>
-                <Form.Control type="text" placeholder="" value={this.state.account} disabled />
-                <Form.Text className="text-muted">
-                  Your wallet address will not be in any way associated with any of your Personally Identifiable Information.
-                </Form.Text>
-              </Form.Group>
-
-              <Form.Text className="card-title h5">Patient parameters</Form.Text>
-              
-              <Form.Group controlId="height">
-                <Form.Label>Height (cm): </Form.Label>
-                <Form.Control type="number" onChange={e => this.handleChange(e)} />
-              </Form.Group>
-              <Form.Group controlId="weight">
-                <Form.Label>Weight (kg): </Form.Label>
-                <Form.Control type="number" onChange={e => this.handleChange(e)} />
-              </Form.Group>
-
+  return (
+    <div>
+      <br></br>
+      <Card>
+        <Card.Body>
+          <Card.Title>Patient Enrollment for Study participation</Card.Title>
+          <Form>
+            <br></br>
+            <Form.Group controlId="walletAccount">
+              <Form.Label>Patient Wallet address: </Form.Label>
+              <Form.Control type="text" placeholder="" value={state.account} disabled />
               <Form.Text className="text-muted">
-                  Please use metric units
+                Your wallet address will not be in any way associated with any of your Personally Identifiable Information.
               </Form.Text>
+            </Form.Group>
 
-              <Form.Group controlId="age">
-                <Form.Label>Age: </Form.Label>
-                <Form.Control type="number" onChange={e => this.handleChange(e)} />
-              </Form.Group>
+            <Form.Text className="card-title h5">Patient parameters</Form.Text>
+            
+            <Form.Group controlId="height">
+              <Form.Label>Height (cm): </Form.Label>
+              <Form.Control type="number" onChange={e => handleChange(e)} />
+            </Form.Group>
+            <Form.Group controlId="weight">
+              <Form.Label>Weight (kg): </Form.Label>
+              <Form.Control type="number" onChange={e => handleChange(e)} />
+            </Form.Group>
 
-              <Form.Group controlId="gender">
-                <Form.Label>Gender: </Form.Label>
-                { GENDERS.map(gender => {
-                  return (<Form.Check 
-                    type="radio" 
-                    label={gender} 
-                    value={gender} 
-                    key={`gender_${gender}`} 
-                    id="gender"
-                    name="gender" 
-                    onChange={e => this.handleChange(e)}/>)
-                }) }
-              </Form.Group>
+            <Form.Text className="text-muted">
+                Please use metric units
+            </Form.Text>
 
-              <Button variant="dark" type="submit" onClick={e => this.onSubmit(e)}>
-                Enroll
-              </Button>
-            </Form>
-          </Card.Body>
-        </Card>
-      </div>
-    );
-  }
+            <Form.Group controlId="age">
+              <Form.Label>Age: </Form.Label>
+              <Form.Control type="number" onChange={e => handleChange(e)} />
+            </Form.Group>
+
+            <Form.Group controlId="gender">
+              <Form.Label>Gender: </Form.Label>
+              { GENDERS.map(gender => {
+                return (<Form.Check 
+                  type="radio" 
+                  label={gender} 
+                  value={gender} 
+                  key={`gender_${gender}`} 
+                  id="gender"
+                  name="gender" 
+                  onChange={e => handleChange(e)}/>)
+              }) }
+            </Form.Group>
+
+            <Button variant="dark" type="submit" onClick={e => onSubmit(e)}>
+              Enroll
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default EnrollForm;
---
+++ src/components/Web3Context.js
@@ -0,0 +1,80 @@
+import React, { createContext, useState, useEffect } from 'react';
+import DoubleBlindStudy from '../abis/DoubleBlindStudy.json'
+import Web3 from 'web3';
+import './App.css';
+
+export const Web3Context = createContext();
+
+export const Web3Provider = props => {
+  const [state, setState] = useState({
+    hasMetamask: false,
+    isMetamaskConnected: false,
+    web3: null,
+    account: null,
+    study: null,
+    address: null,
+    isPatientEnrolled: false,
+    isStudyConcluded: false
+  });
+
+  useEffect(() => {
+    (async () => loadBlockchainData())();
+  }, [state.hasMetamask, state.isMetamaskConnected]);
+
+  const loadBlockchainData = async () => {
+
+    // Check for object exposed by Metamask 
+    if (typeof window.ethereum !== 'undefined') {
+
+      let account = null;
+      let isMetamaskConnected = false;
+      
+      // Try to connect first, if not already connected
+      try {
+        if (!state.isMetamaskConnected) {
+          await window.ethereum.enable();
+        }
+
+        const web3 = new Web3(window.ethereum);
+        const networkId = await web3.eth.net.getId();
+        const accounts = await web3.eth.getAccounts();
+
+        const { abi, networks } = DoubleBlindStudy;
+        const address = networks[networkId] ? networks[networkId].address : null;
+
+        const study = new web3.eth.Contract(abi, address);
+
+        // Make sure we have a main account
+        if (typeof accounts[0] !== 'undefined') {
+          account = accounts[0];
+          isMetamaskConnected = true;
+        }
+
+        setState({
+          hasMetamask: true,
+          web3 : web3,
+          address: address,
+          study: study,
+          account: account,
+          isMetamaskConnected: isMetamaskConnected,
+        });
+      } catch (e) {
+        setState({
+          hasMetamask: true,
+          isMetamaskConnected: isMetamaskConnected,
+        });
+        console.error(e.message);
+      }
+    } else {
+      setState({
+        hasMetamask: false
+      });
+      console.error('MetaMask is not installed.');
+    }
+  }
+
+  const connectMetamask = async () => {
+    await loadBlockchainData();
+  }
+  return <Web3Context.Provider value={{ data: state, connectMetamask: connectMetamask }}>{props.children}</Web3Context.Provider>;
+}
